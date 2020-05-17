25c47da11fdee97da6c733d7c567cb8294eaa608
InvictusMB <invictusmb@gmail.com>
2020-05-17 14:17:41 +0200

    chore(client): Generate openapi client
    > openapi-generator generate -i http://localhost:3000/explorer/openapi.json -g typescript-fetch --skip-validate-spec -o openapi

---
+++ client/src/openapi/apis/index.ts
@@ -0,0 +1,6 @@
+export * from './PingControllerApi';
+export * from './RestaurantControllerApi';
+export * from './RestaurantReviewControllerApi';
+export * from './ReviewControllerApi';
+export * from './ReviewResponseControllerApi';
+export * from './UserControllerApi';
